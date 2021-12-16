a3f75028d384e2dd6fa6ee3f660ab344d3c52c61
Takikoi <khoile.workuser@gmail.com>
2021-12-16 20:35:54 +0700

    Reset folder

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,24 @@
-#include "Package.hpp"
-#include "Game.hpp"
+#include "SFML/Graphics.hpp"
 
 int main()
 {
-    srand(static_cast<unsigned>(time(0)));
-    Game game;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    while (game.running())
+    while (window.isOpen())
     {
-        game.update();
-        game.render();
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
+
     return 0;
 }
